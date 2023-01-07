d606460e49159f37d487082ea81eb8c641a709ae
Likhitha0843 <114049143+Likhitha0843@users.noreply.github.com>
2023-01-07 09:15:29 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,33 @@
+let coffee = 'brew'
+coffee = 'nescafe'
+let email = 'ashish@gmail.com'
+let age = 25;
+
+function grinder(items){
+    console.log('grinding...', items)
+}
+
+function add(num1, num2){
+    console.log('The sum is: ', num1+num2)
+}
+
+
+function joined(){
+    let title = document.getElementById('title');
+    title.innerText = 'Thanks for joining the masterclass live!!';
+    let btn = document.getElementById('btn')
+    btn.style.display = 'none'
+}
+
+function usdToInr(){
+    let usd = document.getElementById('usd').value;
+    let inr = (usd * 82.65);
+    document.getElementById('inr').value = inr;
+}
+
+function split(){
+    let amount = document.getElementById('amount').value;
+    let persons = document.getElementById('persons').value;
+    let result = (amount / persons).toFixed(2);
+    document.getElementById('result').innerText = result
+}
